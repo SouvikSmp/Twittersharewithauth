9df924b8addc235c67c8ed99d143735339c27bc5
arnabesolz <arnab@esolzmail.com>
2015-09-20 12:22:10 +0530

    Initial Commit

---
+++ TwitterShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterShare
+//
+//  Created by Jayati Saha on 20/09/15.
+//  Copyright (c) 2015 Jayati Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterShare
+//
+//  Created by Jayati Saha on 20/09/15.
+//  Copyright (c) 2015 Jayati Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
